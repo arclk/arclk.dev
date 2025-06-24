c4ad114ae2374279ae88e81d5d0c3c3226239c64
arclk <albericoarcangelo@hotmail.it>
2025-06-24 20:33:10 +0200

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Developed by Arcangelo Alberico</h3>
+          <h3>Developed by arclk before LLMs</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           
